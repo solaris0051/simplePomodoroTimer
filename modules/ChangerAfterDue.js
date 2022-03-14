12f310f2ba0478f9657f07273b823f034da03876
Y K <ultraman0713@gmail.com>
2022-03-14 14:48:24 +0900

    changed the timing that a reset button shows up at.

--- modules/ChangerAfterDue.js
+++ modules/ChangerAfterDue.js
@@ -1,8 +1,6 @@
-export const ChangerAfterDue = (dt, id, btn0, btn1, btn2, msg) => {
+export const ChangerAfterDue = (dt, id, btn0, msg) => {
 	const duechanger = setTimeout(() => {
 		btn0.disabled = false;
-		btn1.disabled = false;
-		btn2.disabled = false;
 		console.log(dt);
 		alert(msg);
 		id.className = 'container p-3 bg-success text-white text-center';
