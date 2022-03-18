5bc75fc6c6c17a7a43b6c9725cd07cc6179c65b9
Y K <ultraman0713@gmail.com>
2022-03-18 14:45:22 +0900

    added tiny changes to ChangerAfreDue and ChangerAfterTimeoff in order for Alert to show up after a phone vibrates.

--- modules/ChangerAfterDue.js
+++ modules/ChangerAfterDue.js
@@ -1,9 +1,8 @@
 export const ChangerAfterDue = (dt, id, btn0, msg) => {
 	const duechanger = setTimeout(() => {
 		btn0.disabled = false;
-		console.log(dt);
+		id.className = 'container p-3 bg-success text-white text-center';
 		navigator.vibrate([1000, 500, 900, 400, 800, 300, 1000, 500, 900, 400, 800, 300]);
 		alert(msg);
-		id.className = 'container p-3 bg-success text-white text-center';
 	}, dt * 60 * 1000);
 }
