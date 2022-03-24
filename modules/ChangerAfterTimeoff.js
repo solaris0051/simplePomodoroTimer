7042b26f71dd6abfc6f87ad3bf486aac1e643b36
Y K <ultraman0713@gmail.com>
2022-03-24 21:23:08 +0900

    bug fix-2.1

--- modules/ChangerAfterTimeoff.js
+++ modules/ChangerAfterTimeoff.js
@@ -1,8 +1,5 @@
-export const ChangerAfterTimeoff = (dt, id, btn0, btn1, btn2) => {
+export const ChangerAfterTimeoff = (dt) => {
 	const offchanger = setTimeout(() => {
-		btn0.disabled = false;
-		btn1.disabled = false;
-		btn2.disabled = false;
 		navigator.vibrate([1000, 500, 900, 400, 800, 300, 1000, 500, 900, 400, 800, 300]);
 	}, dt * 60 * 1000);
 	const reloader = setTimeout(() => {
