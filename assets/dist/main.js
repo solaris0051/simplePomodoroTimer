e18b9d0253082e02f6dc1f6df898c703c17cac1c
Y K <docodoco.0713@outlook.com>
2022-06-10 19:57:29 +0900

    Update main.js

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";var __webpack_modules__={471:()=>{eval("\n\n//# sourceURL=webpack://assets/./src/index.js?")}},__webpack_exports__={};__webpack_modules__[471]()})();
+(()=>{"use strict";var __webpack_modules__={471:()=>{eval("\n\n//# sourceURL=webpack://assets/./src/index.js?")}};__webpack_modules__[471]()})();
